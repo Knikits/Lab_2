025b070207c9759c78d640a255768d1c4bd50a48
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-04-22 09:32:52 +0300

    Create structure of states

---
+++ FileState.h
@@ -0,0 +1,17 @@
+#ifndef FILESTATE_H
+#define FILESTATE_H
+
+
+// структурa FileState нужна для представления состояния файла с двумя полями
+struct FileState {
+    bool operator!=(const FileState& other ) { // определяем оператор неравно для того, чтобы уведомлять об изменениях
+        if (this == &other) {
+            return false;
+        }
+        return exists != other.exists || size != other.size;
+    }
+    bool exists;
+    int size;
+};
+
+#endif // FILESTATE_H
