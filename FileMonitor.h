0e6dc2d225a11935dacf4d07f9cceaf9cdc5d1bb
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-04-26 21:30:22 +0300

    Create File monitoring

--- FileMonitor.h
+++ FileMonitor.h
@@ -1,4 +1,17 @@
 #ifndef FILEMONITOR_H
 #define FILEMONITOR_H
+#include <iostream>
+#include "Observable.h"
+#include "FileObserver.h"
+
+class FileMonitor : public Observable< FileObserver > {
+private:
+    std::string m_fileName;
+    FileState m_prevFileState;
+    static FileState getFileState (const std::string& fileName);
+public:
+    FileMonitor(const char* fileName);
+    void checkFile();
+};
 
 #endif // FILEMONITOR_H
