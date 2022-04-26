2afb76e5f1fa830d1199e8f506ef10e944c88b07
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-04-26 20:56:49 +0300

    Protected for observable

--- Observable.h
+++ Observable.h
@@ -3,6 +3,8 @@
 #include <set>
 #include <vector>
 
+using namespace std;
+
 //тут класс который будет регистрировать изменения в классе Observer
 template<class Observer>
 class Observable {
@@ -12,12 +14,27 @@ private:
         Observer* observer;
     };
     int m_count;
+    set< Observer* > m_observers;
+    vector< ObserverRequest > m_requests;
 protected:
     Observable() : m_count(0) {}
+    template< typename F, typename... Args >
+    void notifyObservers( F f, Args... args ) {
+        m_count++;
+        for( Observer* o : m_observers) {
+            ( o->*f ) (args... );
+        };
+        if (m_count == 0) {
+            for( const ObserverRequest& r : m_requests ) {
+                ( this->*r.operation )( r.observer );
+            };
+            m_requests.clean();
+        }
+    }
 public:
     virtual ~Observable() {}
-    void registerObserver () {}
-    void unregisterObserver () {}
+    void registerObserver (Observer* observer);
+    void unregisterObserver (Observer* observer);
 };
 
 #endif // OBSERVABLE_H
