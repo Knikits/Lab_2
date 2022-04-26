3583cd95880f4111e76d23220055c2ce4c43b2d5
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-04-26 18:42:04 +0300

    Struct Observable

--- Observable.h
+++ Observable.h
@@ -3,4 +3,15 @@
 
 //тут класс который будет регистрировать изменения в классе Observer
 
+class Observable {
+private:
+    int m_count;
+protected:
+    Observable() : m_count(0) {}
+public:
+    virtual ~Observable() {}
+    void registerObserver () {}
+    void unregisterObserver () {}
+};
+
 #endif // OBSERVABLE_H
