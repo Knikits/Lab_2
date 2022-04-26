078c6ed0379fe1be61eb59922976c03b805838ff
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-04-26 18:54:24 +0300

    Add a struct of request observer

--- Observable.h
+++ Observable.h
@@ -1,10 +1,16 @@
 #ifndef OBSERVABLE_H
 #define OBSERVABLE_H
+#include <set>
+#include <vector>
 
 //тут класс который будет регистрировать изменения в классе Observer
-
+template<class Observer>
 class Observable {
 private:
+    struct ObserverRequest {
+        void (Observable < Observer >::*operation) (Observer*);
+        Observer* observer;
+    };
     int m_count;
 protected:
     Observable() : m_count(0) {}
