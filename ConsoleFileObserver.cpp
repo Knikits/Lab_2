b9688389748c8261a9fdf0a4c3b5d8991897fc6a
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-04-26 18:06:26 +0300

    Realisation state file

--- ConsoleFileObserver.cpp
+++ ConsoleFileObserver.cpp
@@ -1,4 +1,12 @@
+#include <iostream>
 #include "ConsoleFileObserver.h"
+
+using namespace std;
 
 ConsoleFileObserver::~ConsoleFileObserver(){}
 
+void ConsoleFileObserver::onFileChanged(const FileState &state) {
+    cout << "Does the file exist and is not empty? " << endl;
+    cout << "Exists: " << state.exists << endl;
+    cout << "Size: " << state.size << endl;
+}
