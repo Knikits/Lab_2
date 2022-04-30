f1814e30b9350f364369f86d36b7d4bb74cfe9cb
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-04-30 23:34:40 +0300

    Realisation function of Info

--- IObserver.h
+++ IObserver.h
@@ -18,4 +18,20 @@ public:
     virtual void update(const Info&) = 0;
 };
 
+bool Info::operator==(const Info & info)
+{
+    return !(this->operator!=(info));
+}
+
+bool Info::operator!=(const Info& info)
+{
+    return !((info.exist & exist) && (size == info.size));
+}
+
+void Info::operator=(const Info& info)
+{
+    this->size = info.size;
+    this->exist = info.exist;
+}
+
 #endif // IOBSERVER_H
