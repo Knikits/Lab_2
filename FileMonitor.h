238e0a3767866f7df71246bca5fcc2e0cd21b8c6
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-05-15 23:28:13 +0300

    Comment 1

--- FileMonitor.h
+++ FileMonitor.h
@@ -3,21 +3,25 @@
 #include "IObserver.h"
 #include <QString>
 
+//Info - класс наследник класса IInfo, хранящий информацию о файле
+
 class Info : public IInfo
 {
 public:
-    bool operator==(const Info&);
-    bool operator!=(const Info&);
-    void operator=(const Info&);
-    QString String() const;
-    bool exist;
-    int size;
+    bool operator==(const Info&); // оператор равно
+    bool operator!=(const Info&); // оператор не равно
+    void operator=(const Info&); // оператор присвоения
+    QString String() const; // перевод полей в QString
+    bool exist; // факт существования
+    int size; // размер файла
 };
+
+//FileMonitor - класс наследник класса IObserver
 
 class FileMonitor : public IObserver
 {
 public:
-    void update(IInfo*);
+    void update(IInfo*); // обновление информации о файле
 };
 
 #endif // FILEMONITOR_H
--- IObserver.h
+++ IObserver.h
@@ -1,21 +1,23 @@
 #ifndef IOBSERVER_H
 #define IOBSERVER_H
-
 #include <QString>
+
+//IInfo - класс (интерфейс), хранящий в себе абстрактную информацию
 
 class IInfo
 {
 public:
-    virtual ~IInfo(){}
-    virtual QString String() const = 0;
+    virtual ~IInfo(){} // деструктор
+    virtual QString String() const = 0; // переводит данные в QString
 };
 
+//IObserver - класс (интерфейс) для класса наблюдателей
 
 class IObserver
 {
 public:
-    virtual ~IObserver() {}
-    virtual void update(IInfo*) = 0;
+    virtual ~IObserver() {} // деструктор
+    virtual void update(IInfo*) = 0; // обновление информации
 };
 
 #endif // IOBSERVER_H
