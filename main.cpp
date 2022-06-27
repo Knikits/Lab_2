75bfda61b795a54860d9ceb5971488eaf46a2312
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-06-27 15:26:27 +0300

    Test for lab_2

--- main.cpp
+++ main.cpp
@@ -22,4 +22,72 @@ int main(int argc, char *argv[])
         std::this_thread::sleep_for(std::chrono::milliseconds(1)); //миллисекунда
     }
     return a.exec();
+
+    //протестируем работу с разными условиями
+
+    //когда на файл не подписан подписчик, соответственно, за файлом следить некому!
+    /*
+    QCoreApplication a(argc, argv);
+    File file("D:/Lab_2/file.txt"); // объект, за которым следим
+    FileMonitor check1; // наблюдатель
+    //file.Sub(&check1); // оформляем подписку
+    while(true)
+    {
+        file.checkFile();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1)); //миллисекунда
+    }
+    return a.exec();
+    */
+
+    //когда на файл подписано несколько наблюдателей
+    /*
+    QCoreApplication a(argc, argv);
+    File file("D:/Lab_2/file.txt"); // объект, за которым следим
+    FileMonitor check1; // наблюдатель
+    FileMonitor check2;
+    file.Sub(&check1); // оформляем подписку
+    file.Sub(&check2);
+    while(true)
+    {
+        file.checkFile();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1)); //миллисекунда
+    }
+    return a.exec();
+    */
+    //Как мы видим, каждый наблюдатель будет оповещён об изменениях. Повторюсь, что количество наблюдателей, одновременно взаимодействующих с объектом, ничем не ограничено.
+
+    //подпишем и отпишем сразу же! Всё корректно
+    /*
+    QCoreApplication a(argc, argv);
+    File file("D:/Lab_2/file.txt"); // объект, за которым следим
+    FileMonitor check1; // наблюдатель
+    file.Sub(&check1); // оформляем подписку
+    file.unSub(&check1);
+    while(true)
+    {
+        file.checkFile();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1)); //миллисекунда
+    }
+    return a.exec();
+    */
+
+    /*
+    //попробуем отписать неподписанного наблюдателя, соответственно, никто не будет следить за файлом.
+    QCoreApplication a(argc, argv);
+    File file("D:/Lab_2/file.txt"); // объект, за которым следим
+    FileMonitor check1; // наблюдатель
+    file.unSub(&check1);
+    while(true)
+    {
+        file.checkFile();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1)); //миллисекунда
+    }
+    return a.exec();
+    */
+
+    /*
+    также можно менять число в скобках std::this_thread::sleep_for(std::chrono::milliseconds(1))
+    это частота обновления
+    */
+
 }
